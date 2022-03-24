212a9bab6341def5783b8cdce4298b8183cf396b
CirnoFumo9 <94131088+CirnoFumo9@users.noreply.github.com>
2022-03-23 21:13:34 -0400

    done for ton

--- checkers/src/main.rs
+++ checkers/src/main.rs
@@ -1,11 +1,16 @@
 use std::collections::HashMap;
 
 fn main() {
-    println!("Hewwo~");
+    println!("\n\nHewwo~");
     println!("Initiawizing... pls waitt :3");
 
-    println!("/n/n/tcli-checkers UwU/n
-            /tPress any key to begin");
+    println!(
+        "\n\n\tcli-checkers UwU\n
+            \tPress any key to begin"
+    );
+    //* wait for key press...
+
+    //* Maybe put below into a function to configure/initialize the game.
 
     // The following creates a list of coordinate names and puts them in a vector
     // e.g. 0_0, 3_2, 9_9; where "x-coords_y-coords".
@@ -25,9 +30,53 @@ fn main() {
     println!("{}", default_states.len()); //debug
 
     // The following creates a Hash Map from combining the two previous vectors.
-    // The new hash map stores the following: HashMap<"x_y coordinate", "tile occupancy">
-    let mut stats: HashMap<_,_> = board_coords.iter().zip(default_states.iter()).collect();
+    // The hash map stores the following: HashMap<"x_y coordinate", "tile occupancy">
+    // This will later be used to keep track of the board and its pieces, as well as
+    //   in checking if a given movement option is possible.
+    let mut stats: HashMap<_, _> = board_coords.iter().zip(default_states.iter()).collect();
     println!("{:#?}", stats); //debug
 
+    print_board();
+}
 
+fn stats_config() {}
+
+fn print_board() {
+    println!(
+        "\n
+    ,______ ______ ______ ______ ______ ______ ______ ______ ______ ______,
+    |      |      |      |      |      |      |      |      |      |      |
+ 9  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 8  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 7  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 6  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 5  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 4  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 3  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 2  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 1  |      |      |      |      |      |      |      |      |      |      |
+    |------ ------ ------ ------ ------ ------ ------ ------ ------ ------|
+    |      |      |      |      |      |      |      |      |      |      |
+ 0  |      |      |      |      |      |      |      |      |      |      |
+    '------ ------ ------ ------ ------ ------ ------ ------ ------ ------'
+        0      1      2      3      4      5      6      7      8      9
+
+    foo"
+    );
 }
