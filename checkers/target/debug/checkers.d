d4bb24d9dcff06e10d99a2da6eb711368f1092d1
CirnoFumo9 <94131088+CirnoFumo9@users.noreply.github.com>
2022-03-29 17:39:02 -0400

    rem checkers

--- checkers/target/debug/checkers.d
+++
@@ -1,1 +0,0 @@
-/Users/kaiarntz/vsCodeAll_/rust_projects/rust-checkers/checkers/target/debug/checkers: /Users/kaiarntz/vsCodeAll_/rust_projects/rust-checkers/checkers/src/main.rs
--- checkers/target/debug/deps/checkers-a564e50f1db03eb6.d
+++
@@ -1,5 +0,0 @@
-/home/runner/rust-checkers/checkers/target/debug/deps/checkers-a564e50f1db03eb6: src/main.rs
-
-/home/runner/rust-checkers/checkers/target/debug/deps/checkers-a564e50f1db03eb6.d: src/main.rs
-
-src/main.rs:
