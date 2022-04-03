94149eed55abbad06d71b797723e21a5e13f3a2c
CirnoFumo9 <94131088+CirnoFumo9@users.noreply.github.com>
2022-04-03 02:54:51 -0400

    final commit

--- structs_checkers/target/debug/structs_checkers.d
+++ structs_checkers/target/debug/structs_checkers.d
@@ -1,1 +1,1 @@
-/Users/kaiarntz/vsCodeAll_/rust_projects/rust-checkers/structs_checkers/target/debug/structs_checkers: /Users/kaiarntz/vsCodeAll_/rust_projects/rust-checkers/structs_checkers/src/main.rs
+/Users/kaiarntz/things-to-keep-in-home-dir/vsCodeAll_/rust_projects/rust-checkers/structs_checkers/target/debug/structs_checkers: /Users/kaiarntz/things-to-keep-in-home-dir/vsCodeAll_/rust_projects/rust-checkers/structs_checkers/src/main.rs
