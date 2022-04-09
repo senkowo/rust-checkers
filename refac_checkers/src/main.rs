68c3364905368325b2f855360ed6d350e88ac572
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-04-09 22:24:34 +0000

    fin upd

--- refac_checkers/src/main.rs
+++ refac_checkers/src/main.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Tile {
     P1(Stack),
     P2(Stack),
     Emp,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, PartialEq)]
 enum Stack {
     Single,
     Double,
@@ -19,13 +19,13 @@ enum PlayerTurn {
     P2,
 }
 
-const X_LEN = 8;
-const Y_LEN = 8;
+const X_LEN: u8 = 8;
+const Y_LEN: u8 = 8;
 
 impl Tile {
     fn init_fill_hashmap(stats: &mut HashMap<(u8, u8), Tile>) {
-        for y in 1..X_LEN {
-            for x in 1..X_LEN {
+        for y in 1..=Y_LEN {
+            for x in 1..=X_LEN {
                 stats.insert(
                     (x, y),
                     if ((x + (y % 2)) % 2) == 1 {
@@ -35,53 +35,47 @@ impl Tile {
                             _ => Tile::Emp,
                         }
                     } else {
-                        Tile::Emp,
-                    }
-                )
+                        Tile::Emp
+                    },
+                );
             }
         }
+    }
+    fn change_tile_st(&mut self, variant: Tile) {
+        *self = variant;
+    }
+    fn change_tile_state(
+        stats: &mut HashMap<(u8, u8), Tile>,
+        location: (u8, u8),
+        variant: Tile,
+    ) {
+        *stats.get_mut(&location).expect("OwO whats this?") = variant;
     }
 }
 
 fn main() {
-
     introduction();
 
     let mut stats: HashMap<(u8, u8), Tile> = HashMap::new();
-    
+
     Tile::init_fill_hashmap(&mut stats);
 
-    println!(
-        "test: {:#?}\n{:#?}",
-        match stats.get(&(0, 0)) {
-            None => panic!("OwO whats this? Failed to .get() stats"),
-            Some(i) => i,
-        },
-        stats,
-    );
     //stats.get(&(0, 0)).unwrap().init_fill(&mut stats);
     // Tile::update(&mut stats, (2, 3), &Tile::P1(Stack::Double));
     // stats.insert((4, 4), Tile::Emp);
-    
-    println!("{:#?}", stats);
+
+    // i could just change the end with a equals Tile::Emp...
+    stats.get_mut(&(1, 1)).unwrap().change_tile_st(Tile::Emp);
+    // simplified, abstraction, maybe put the above in the function?
+    Tile::change_tile_state(&mut stats, (8, 8), Tile::Emp);
+
+    println!("{:?}", stats);
 }
 
-fn introduction() {
-    
-}
+fn introduction() {}
 
 impl Tile {
-//    fn init_fill(&self, &mut ) {
-    //    stats
-//    }
-    fn empty(&self, x_and_y: (u8, u8)) -> Tile {
-        Tile::P1(Stack::Double)
-    }
-    fn update(
-        stats: &mut HashMap<(u8, u8), Tile>,
-        xy: (u8, u8),
-        v: &Tile,
-    ) {
+    fn update(stats: &mut HashMap<(u8, u8), Tile>, xy: (u8, u8), v: &Tile) {
         //*stats.entry((xy.0, xy.1)).or_insert(Tile::Emp) = *v;
         stats.insert((xy.0, xy.1), Tile::P1(Stack::Double));
     }
---
+++ refac_checkers/target/debug/deps/refac_checkers-6da96bd1dabbdb2b.d
@@ -0,0 +1,5 @@
+/home/runner/rust-checkers/refac_checkers/target/debug/deps/refac_checkers-6da96bd1dabbdb2b.rmeta: src/main.rs
+
+/home/runner/rust-checkers/refac_checkers/target/debug/deps/refac_checkers-6da96bd1dabbdb2b.d: src/main.rs
+
+src/main.rs:
