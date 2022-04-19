79017e409fba2a6e89ca346e464f8e70ea2cfe7c
KAI ARNTZ <94131088+CirnoFumo9@users.noreply.github.com>
2022-04-19 11:24:05 +0000

    upd

--- refac_checkers/target/debug/refac_checkers.d
+++ refac_checkers/target/debug/refac_checkers.d
@@ -1,1 +1,1 @@
-/Users/kaiarntz/programming-socks/projects/rust/rust-checkers/refac_checkers/target/debug/refac_checkers: /Users/kaiarntz/programming-socks/projects/rust/rust-checkers/refac_checkers/src/main.rs
+/home/runner/rust-checkers/refac_checkers/target/debug/refac_checkers: /home/runner/rust-checkers/refac_checkers/src/main.rs
